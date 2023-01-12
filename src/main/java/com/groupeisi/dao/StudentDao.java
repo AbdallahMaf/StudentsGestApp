a2d191868d1ddc7ae3bc051f9dee2c9e86d9ef01
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-01-12 14:27:11 +0000

    StudentDaoCRUD_SQL_QUERY

--- src/main/java/com/groupeisi/dao/StudentDao.java
+++ src/main/java/com/groupeisi/dao/StudentDao.java
@@ -7,7 +7,16 @@ public class StudentDao {
 	public String password = "";
 	public String jdbcDriver = "com.mysql.jdbc.Driver";
 	
-	private static final String INSERT_STUDENT_SQL = "INSERT INTO student" + " (name, )";
+	private static final String INSERT_STUDENT_SQL = "INSERT INTO students" + 
+			" (nom, prenom, email, date, classe) VALUES " + " (?, ?, ?, ?, ?);";
+	
+	private static final String SELECT_STUDENT_BY_ID = "SELECT id, nom, prenom, email, date, classe FROM students WHERE id =?;";
+	
+	private static final String SELECT_ALL_STUDENTS = "SELECT * FROM students;";
+	
+	private static final String DELETE_STUDENT_SQL = "DELETE FROM students WHERE id =?;";
+	
+	private static final String UPDATE_STUDENT_SQL = "UPDATE students SET nom = ?, prenom = ?, email = ?, date = ?, classe = ?;";
 	
 	
 	
