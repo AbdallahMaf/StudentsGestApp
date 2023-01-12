d23fe35ba49a4a62a4b928f6146bee583f076ac8
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-01-12 13:38:32 +0000

    StudentDaoStringConnectionDB

--- src/main/java/com/groupeisi/dao/StudentDao.java
+++ src/main/java/com/groupeisi/dao/StudentDao.java
@@ -2,4 +2,10 @@ package com.groupeisi.dao;
 
 public class StudentDao {
 
+	public String url = "jdbc:mysql://localhost:3306/studentgest?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public String username = "root";
+	public String password = "";
+	public String jdbcDriver = "com.mysql.jdbc.Driver";
+	
+	
 }
