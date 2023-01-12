de312f3bb90223dfa88dc972ef797f03c9dbd547
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-01-12 16:29:06 +0000

    prinlSQLException

--- src/main/java/com/groupeisi/dao/StudentDao.java
+++ src/main/java/com/groupeisi/dao/StudentDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Iterator;
 
 import com.groupeisi.entities.Student;
 
@@ -59,13 +60,27 @@ public class StudentDao {
 			}
 	}
 
-	private void printSQLException(SQLException e) {
-		
-	}
 	
 	//SELECT STUDENT BY ID
 	//SELECT ALL STUDENTS
 	//UPDATE STUDENT
 	//DELETE STUDENT
 	
+	
+	private void printSQLException(SQLException ex) {
+		for(Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("SQLError code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while(t != null ) {
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
+
+			}
+		}
+	}
 }
