17b711ca8ce9a5942d28fb99825bf0cb4d0a9bb8
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-01-19 02:18:31 +0000

    Insert Method ServletStudent

--- src/main/java/com/groupeisi/servlet/ServletStudent.java
+++ src/main/java/com/groupeisi/servlet/ServletStudent.java
@@ -8,6 +8,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.groupeisi.dao.StudentDao;
+import com.groupeisi.entities.Student;
+
 import jakarta.servlet.RequestDispatcher;
 
 /**
@@ -16,6 +19,7 @@ import jakarta.servlet.RequestDispatcher;
 @WebServlet("/")
 public class ServletStudent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private StudentDao studentDao;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,7 +33,7 @@ public class ServletStudent extends HttpServlet {
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init() throws ServletException {
-		
+		studentDao = new StudentDao();
 	}
 
 	/**
@@ -45,34 +49,47 @@ public class ServletStudent extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getServletPath();
-	switch(action) 
-	{
-	case "/new":
-		showNewForm(request, response);
-		break; 
 		
-	case "/insert":
-		break; 
-		
-	case "/delete":
-		break; 
-		
-	case "/edit":
-		break; 
-		
-	case "/update":
-		break; 
-		
-		default:
-			break;
+		switch(action) {
+		case "/new":
+			showNewForm(request, response);
+			break; 
 			
+		case "/insert":
+			insertStudent(request, response);
+			break; 
+			
+		case "/delete":
+			break; 
+			
+		case "/edit":
+			break; 
+			
+		case "/update":
+			break; 
+			
+			default:
+				break;
+				
+		}
 	}
-	
 
 	private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("student-form.jsp");
-		dispatcher.forward(request, response);
-		
+		dispatcher.forward(request, response);	
 	}
+	
+	// Insert
+	private void insertStudent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String date = request.getParameter("date");
+		String classe = request.getParameter("classe");
+		Student newStudent = new Student(0, nom, prenom, email, date, classe);
+			studentDao.insertStudent(newStudent);
+	}
+	
+	
 
 }
